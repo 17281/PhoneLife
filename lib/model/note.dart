1d7a23860b156ced22842d546af2ae8d075ad7f3
17281 <17281@burnside.school.nz>
2021-06-25 13:51:38 +1200

    Created an data insert method (Means that adding things into database is now possible)

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -3,6 +3,12 @@ final String tableNotes = 'notes';
 
 //contents created in SQL
 class NoteFields {
+  static final List<String> values = [
+    ///add all fields
+    id, isImportant, number, title, description,time
+  ];
+
+
   static final String id = '_id';
   static final String isImportant = 'isImportant';
   static final String number = 'number';
@@ -21,6 +27,7 @@ class Note {
   final String description;
   final DateTime createdTime;
 
+
   const Note ({
     this.id,
     required this.isImportant,
@@ -30,4 +37,38 @@ class Note {
     required this.createdTime,
 });
 
+  //copies note objects and changes items within database
+  Note copy({
+    int? id,
+    bool? isImportant,
+    int? number,
+    String? title,
+    String? description,
+    DateTime? createdTime,
+
+}) =>
+  Note(
+    id: id ?? this.id,
+    isImportant: isImportant ?? this.isImportant,
+    number: number ?? this.number,
+    title: title ?? this.title,
+    description: description ?? this.description,
+    createdTime: createdTime ?? this.createdTime,
+  );
+
+  // Map to allow data to be inserted into sql
+  Map<String, Object?>toJson() => {
+    //a map of key values
+    //Table name:Key:corrisponding value
+    NoteFields.id: id,
+    NoteFields.title: title,
+    NoteFields.description: description,
+    NoteFields.number: number,
+
+    //Bool and DateTime needs conversion before inserted into sql
+    NoteFields.isImportant: isImportant ? 1 : 0,
+
+    //converts time to string
+    NoteFields.time: createdTime.toIso8601String(),
+  };
 }
