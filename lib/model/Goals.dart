e6de3015d0cddc1e124cf2bc0421bfc3369a7299
17281 <17281@burnside.school.nz>
2021-07-14 11:04:13 +1200

    Adding UserContent table items for updating and adding data into database

--- lib/model/Goals.dart
+++ lib/model/Goals.dart
@@ -1,19 +1,18 @@
-//Name of table created , This part is basically SQA
-import 'package:sqflite/sqflite.dart';
-
+//Name of table created , This part is basically SQA but with json conversions
 final String userTable = 'User';
 
 //contents created in SQL
 class UserFields {
   static final List<String> values = [
     ///add all fields
-    id, isImportant, name,time
+    id, isImportant, name,time, description
   ];
 
 
   static final String id = '_id';
   static final String isImportant = 'isImportant';
   static final String name = 'name';
+  static final String description = 'description';
   static final String time = 'time';
 }
 
@@ -23,6 +22,7 @@ class UserContent {
   final int? id;
   final bool isImportant;
   final String name;
+  final String description;
   final DateTime createdTime;
 
 
@@ -30,6 +30,7 @@ class UserContent {
   const UserContent ({
     this.id,
     required this.isImportant,
+    required this.description,
     required this.name,
     required this.createdTime,
 });
@@ -40,6 +41,7 @@ class UserContent {
     int? id,
     bool? isImportant,
     String? name,
+    String? description,
     DateTime? createdTime,
 
 }) =>
@@ -48,6 +50,7 @@ class UserContent {
     id: id ?? this.id,
     isImportant: isImportant ?? this.isImportant,
     name: name ?? this.name,
+    description: description ?? this.description,
     createdTime: createdTime ?? this.createdTime,
   );
 
@@ -55,7 +58,7 @@ class UserContent {
   static UserContent fromJson(Map<String, Object?> json) => UserContent(
     id: json[UserFields.id] as int?,
     name: json[UserFields.name] as String,
-
+    description: json[UserFields.description] as String,
     //specials cases that need extra converting i.e bool and tim
     createdTime: DateTime.parse(json[UserFields.time] as String),
     isImportant: json[UserFields.isImportant] == 1,
@@ -69,7 +72,7 @@ class UserContent {
     //Table name:Key:corrisponding value
     UserFields.id: id,
     UserFields.name: name,
-
+    UserFields.description: description,
     //Bool and DateTime needs conversion before inserted into sql
     UserFields.isImportant: isImportant ? 1 : 0,
 
--- lib/page/Edit_Goals.dart
+++ lib/page/Edit_Goals.dart
@@ -2,3 +2,19 @@ import 'package:phoneapp/db/User_database.dart';
 import 'package:flutter/material.dart';
 import 'package:phoneapp/model/Goals.dart';
 
+class AddEditGoalPage extends StatefulWidget {
+  final UserContent? goal;
+  const AddEditGoalPage({Key? key, this.goal}) : super(key: key);
+  @override
+  _AddEditGoalPageState createState() => _AddEditGoalPageState();
+}
+
+class _AddEditGoalPageState extends State<AddEditGoalPage>{
+  //creates a validation method for when users insert data into database
+  final _formkey = GlobalKey<FormState>();
+  late bool isImportant;
+  late int number;
+  late String title;
+  late String description;
+
+}
