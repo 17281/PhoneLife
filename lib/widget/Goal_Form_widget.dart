2829308a5f4d5dbc369810725b3b358c5b7910d2
17281 <17281@burnside.school.nz>
2021-07-14 11:49:23 +1200

    Using the _formkey, to validate inserting methods (ie. name can't be null, etc....)

--- lib/widget/Goal_Form_widget.dart
+++ lib/widget/Goal_Form_widget.dart
@@ -7,4 +7,26 @@ class GoalFormWidget extends StatelessWidget {
   final ValueChanged<bool> onChangedImportant;
   final ValueChanged<String> onChangedName;
   final ValueChanged<String> onChangedDescription;
+
+//Validating methods of inserting, ie. Name can't be null
+  const GoalFormWidget({
+    Key? key,
+    this.isImportant = false,
+    this.name = '',
+    this.description = '',
+    required this.onChangedImportant,
+    required this.onChangedName,
+    required this.onChangedDescription,
+  }) : super(key: key);
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
 }
+
