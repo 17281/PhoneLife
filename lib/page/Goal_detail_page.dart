3f7c7311cb16d1331da666663b5f4e6fdc287291
17281 <17281@burnside.school.nz>
2021-07-16 15:00:20 +1200

    making the UI and function/editing of goals in a 'detail page'

---
+++ lib/page/Goal_detail_page.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:phoneapp/db/User_database.dart';
+import 'package:phoneapp/model/Goals.dart';
+import 'package:phoneapp/page/Edit_Goals.dart';
