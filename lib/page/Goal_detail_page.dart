1e6cab96246bbfef1e01854cab1a6c4dd8b51292
17281 <17281@burnside.school.nz>
2021-07-16 15:04:51 +1200

    Display detail page where the input key is valid and where goalID = the one pressed

--- lib/page/Goal_detail_page.dart
+++ lib/page/Goal_detail_page.dart
@@ -3,3 +3,15 @@ import 'package:intl/intl.dart';
 import 'package:phoneapp/db/User_database.dart';
 import 'package:phoneapp/model/Goals.dart';
 import 'package:phoneapp/page/Edit_Goals.dart';
+
+
+class GoalDetailPage extends StatefulWidget {
+  //the goal ID returned has to be INT
+  ///display goals where goalID = ?
+  final int goalID;
+
+  const GoalDetailPage ({
+    Key? key,
+    required this.goalID,
+}) :super(key: key);
+}
