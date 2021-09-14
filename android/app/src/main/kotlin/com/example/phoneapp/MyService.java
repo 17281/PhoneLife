6661b7bdc3593d993ec588274078e191ef363dc9
17281 <17281@burnside.school.nz>
2021-09-14 12:44:37 +1200

    adding background notification setting and icon

--- android/app/src/main/kotlin/com/example/phoneapp/MyService.java
+++ android/app/src/main/kotlin/com/example/phoneapp/MyService.java
@@ -1,12 +1,29 @@
 package com.example.phoneapp;
 
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            NotificationCompat.Builder builder = new NotificationCompat.Builder
+                    (this, "messages")
+                    .setContentText("this is running in the back")
+                    .setContentTitle("Flutter Background")
+                    .setSmallIcon(R.drawable.ic_andriod_black_24dp);
+        }
+    }
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
